ef5b335559203fb850984ab9cc468eec3d5d4de2
MateoKania <mateotomaskania@gmail.com>
2026-03-05 17:14:49 +0100

    Update footer.jsx
    add return to home in fotter

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
         <footer>
           <p className="flex text-gray-400 items-center justify-center w-8/12  mx-auto">
             © 2025 Mateusz Tomasz Kania · Todos los derechos reservados ·
-            Valencia, España
+            Valencia, España · <a href="#home">Inicio</a>
           </p>
         </footer>
       </section>
